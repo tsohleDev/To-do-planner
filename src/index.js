4035c17a26f41eb46ceb0a5cf46a6ca5bc9da83f
tsohleDev <mokhemisitsohle@gmail.com>
2022-12-08 06:24:04 +0200

    Style the html with css

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import "./style.css";
 import Task from "./task.js";
 
 const section = document.querySelector('#list')
-let list = [new Task(0, 'Read', false), new Task(1, 'Eat', true),new Task(2, 'Write', false)]
+let list = [new Task(5, 'Read', false), new Task(1, 'Eat', true),new Task(2, 'Write', false)]
 
 const loadItems = () => {
+    list.sort((a, b) => a.index - b.index)
+
     list.forEach(task => {
         const {i, description, com} = task
 
@@ -13,8 +15,8 @@ const loadItems = () => {
         display[0].type = 'checkbox'
         display[1].readOnly = true
         display[1].value = description
-        console.log(display[1].value);
-        display.forEach(element => {
+        display[2].innerHTML = '<i class="fa-solid fa-ellipsis-vertical"></i>'
+         display.forEach(element => {
             div.appendChild(element)
         });
         section.appendChild(div)
