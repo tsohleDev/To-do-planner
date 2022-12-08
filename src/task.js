dfbc5821b9d870a3cffb80caa6b04167466be3ab
tsohleDev <mokhemisitsohle@gmail.com>
2022-12-08 12:37:56 +0200

    Fix linters

--- src/task.js
+++ src/task.js
@@ -1,9 +1,9 @@
 class Task {
-    constructor(index, description, completed) {
-        this.index = index
-        this.description = description
-        this.completed = completed
-    }
+  constructor(index, description, completed) {
+    this.index = index;
+    this.description = description;
+    this.completed = completed;
+  }
 }
 
-export default Task
+export default Task;
