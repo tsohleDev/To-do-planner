6e45516cd8884751f98e30fde93fcae60692408f
tsohleDev <mokhemisitsohle@gmail.com>
2022-12-09 16:49:49 +0200

    Fix Clear all comple functionality

--- src/checkBox.js
+++ src/checkBox.js
@@ -6,6 +6,11 @@ class CheckBox {
 
     this.node.addEventListener('change', () => {
       task.completed = this.node.checked;
+      if (task.completed) {
+        task.textBox.style.textDecoration = 'line-through';
+      } else {
+        task.textBox.style.textDecoration = 'none';
+      }
       list.store();
     });
   }
