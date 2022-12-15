d1a48fa4240d48b71483666e80db50b3eb894cf4
Lucas David Erkana <lderkana21x@gmail.com>
2022-12-15 12:05:48 +0200

    add delete test function

---
+++ tests/delete.test.js
@@ -0,0 +1,16 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import Task from "../src/task.js"
+import ToDoList from "../src/list.js"
+
+test('delete one task to the list', () => {
+    const section = document.createElement('div')
+    const toDoList = new ToDoList(section, [])
+    const task = new Task('write a task', false, toDoList)
+    toDoList.add(task)
+    toDoList.remove(task)
+
+    expect(toDoList.array.length).toBe(0)
+})
